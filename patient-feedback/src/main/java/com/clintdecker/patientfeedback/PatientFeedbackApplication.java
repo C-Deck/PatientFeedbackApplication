dba0d7b34e03c31b198499be610d30c05e7e600a
unknown <cdecker18@gmail.com>
2021-12-04 19:49:33 -0800

    Adding backend

---
+++ patient-feedback/src/main/java/com/clintdecker/patientfeedback/PatientFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.clintdecker.patientfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientFeedbackApplication.class, args);
+	}
+
+}
---
+++ patient-feedback/src/test/java/com/clintdecker/patientfeedback/PatientFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clintdecker.patientfeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
